ab33e3f21efc89618dd945f9377823f6d93e486d
jjsalva <jjsalva00@gmail.com>
2020-09-17 15:46:26 +0200

    intento de transicion

--- funciones.js
+++ funciones.js
@@ -2,32 +2,19 @@ function mostrar(id) {
     let midiv = document.getElementById(id);
     midiv.animationduration = "2s";
     if (midiv.style.display == 'none'){
+
         midiv.style.display = 'block';
-        midiv.style.left="0%";
+        midiv.style.width="auto";
         /*div.style.left="0px";*/
     }else{
         midiv.style.display = 'none';
-        midiv.style.left="-100%";
+        midiv.style.width="auto";
     }
 }
 
 function cerrar(id) {
     let midiv = document.getElementById(id);
+    midiv.style.width="0px";
     midiv.style.display = 'none';
-    midiv.style.left="-100%";
 }
 
-(function(){
-    let  elementos = document.getElementsByClassName("actividad");
-    for (i = 0; i < elementos.length; i++) {
-        let elem1 = document.forms[0];
-
-
-        var nuevo_parrafo = document.createElement('p').appendChild(document.createTextNode('Nuevo párrafo.'));
-
-        var segundo_p = elem1;
-
-        document.getElementById('padre').insertBefore(nuevo_parrafo,segundo_p);
-
-      }
-})();
