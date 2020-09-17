7c4e85d615802a8141d6b8793c02688a12d9fe12
jjsalva <jjsalva00@gmail.com>
2020-09-17 15:19:22 +0200

    añado pagina con certificados

--- funciones.js
+++ funciones.js
@@ -1,14 +1,33 @@
 function mostrar(id) {
-    let div = document.getElementById(id);
-    div.animationduration = "2s";
-    if (div.style.display == 'none'){
-        div.style.display = 'block';
+    let midiv = document.getElementById(id);
+    midiv.animationduration = "2s";
+    if (midiv.style.display == 'none'){
+        midiv.style.display = 'block';
+        midiv.style.left="0%";
+        /*div.style.left="0px";*/
     }else{
-        div.style.display = 'none';
+        midiv.style.display = 'none';
+        midiv.style.left="-100%";
     }
 }
 
 function cerrar(id) {
-    let div = document.getElementById(id);
-    div.style.display = 'none';
+    let midiv = document.getElementById(id);
+    midiv.style.display = 'none';
+    midiv.style.left="-100%";
 }
+
+(function(){
+    let  elementos = document.getElementsByClassName("actividad");
+    for (i = 0; i < elementos.length; i++) {
+        let elem1 = document.forms[0];
+
+
+        var nuevo_parrafo = document.createElement('p').appendChild(document.createTextNode('Nuevo párrafo.'));
+
+        var segundo_p = elem1;
+
+        document.getElementById('padre').insertBefore(nuevo_parrafo,segundo_p);
+
+      }
+})();
